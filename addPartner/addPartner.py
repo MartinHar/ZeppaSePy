bdec5322d9e5c956acce8080abcb75669c2be6cd
martinhar <martin.harutyunyan92@gmail.com>
2022-08-27 14:51:45 +0400

    27.08.2022

--- addPartner/addPartner.py
+++ addPartner/addPartner.py
@@ -4,27 +4,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import random
+import names
 from os import path
 
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-
-
 driver.implicitly_wait(10)
-
-
-make_arm_word = 'աբգդեզէ'
-
-
-def generate_random_arm_word(make_arm_word, n):
-    return ''.join(random.choices(make_arm_word, k=n))
-
-
-make_eng_word = 'abcdefg'
-
-
-def generate_random_eng_word(make_eng_word, n):
-    return ''.join(random.choices(make_eng_word, k=n))
 
 
 make_rand_number = '0123456789'
@@ -39,6 +24,52 @@ pdf = path.abspath(path.join(path.dirname(__file__), '2.pdf'))
 png = path.abspath(path.join(path.dirname(__file__), '3.png'))
 main_file = path.abspath(path.join(path.dirname(__file__), '4.zip'))
 
+arm_street_names = ['Աբելյան', 'Աբովյան', 'Ագաթանգեղոսի', 'Ազատամարտիկների', 'Ազատության պողոտա', 'Աթենքի',
+                    'Աթոյան', 'Ալեք Մանուկյան', 'Ալիխանյան', 'Աղայան', 'Աղյուսագործների', 'Ամիրյան', 'Այասի',
+                    'Անտառային', 'Անրի Վեռնոյի', 'Գյուլբենկյան', 'Գոհար Գասպարյան', 'Գրիգոր Հարությունյան',
+                    'Գրիգոր Տեր-Գրիգորյան', 'Դեղատան', 'Դերենիկ Դեմիրճյան', 'Եզնիկ Կողբացու', 'Եկմալյան',
+                    'Երվանդ Քոչարի', 'Զավարյան', 'Զարոբյան', 'Զաքյան', 'Էրեբունու', 'Թաիրովի', 'Ռոստովյան',
+                    'Ռուսթավելու', 'Սայաթ-Նովայի պողոտա', 'Սասունցի Դավթի', 'Սարալանջի', 'Սարմենի', 'Սարյան',
+                    'Սերգեյ Փարաջանովի', 'Սևանի', 'Սիլվա Կապուտիկյան', 'Սիմեոն Երևանցու', 'Ծատուրյան',
+                    'Ծխախոտագործների', 'Ծովակալ Իսակովի պողոտա', 'Կալենցի', 'Կասյան', 'Կարեն Դեմիրճյան',
+                    'Կիևյան', 'Կոմիտասի պողոտա', 'Տիգրան Մեծի պողոտա', 'Տոլստոյի', 'Տպագրիչների', 'Ցախի',
+                    'Փավստոս Բուզանդի', 'Քաջազնունու', 'Քոչինյան', 'Քրիստափորի', 'Օստրովսկու', 'Օրբելի եղբայրների',
+                    'Ֆիզկուլտուրնիկների', 'Ֆիրդուս']
+
+arm_surnames = ['Հովհաննիսյան', 'Սարգսյան', 'Գրիգորյան', 'Հարությունյան', 'Գրիգորյան', 'Սարիբեկյան', 'Խաչատրյան',
+                'Ադամյան', 'Խաչատուրյան', 'Հակոբյան', 'Պետրոսյան', 'Կարապետյան', 'Գևորգյան',
+                'Խորխորունի', 'Մամիկոնյան', 'Ղարամանյան', 'Ծառուկյան', 'Մկրտչյան', 'Վարդանյան', 'Ղազարյան',
+                'Գասպարյան', 'Մանուկյան', 'Մանուչարյան', 'Մակիչյան', 'Ավետիսյան', 'Պողոսյան', 'Սահակյան',
+                'Պեպելյան', 'Սիմոնյան', 'Արիստակեսյան', 'Բագրատյան', 'Բզնունի', 'Յայլոյան', 'Բաղդասարյան']
+
+
+arm_girl_name = ['Ագապի', 'Ազատուհի', 'Այծեմնիկ', 'Անի', 'Անթառամ', 'Աշխէն', 'Ասթինէ', 'Աստղիկ', 'Արեգնազան',
+                 'Արշակուհի', 'Արտեմիս', 'Արփենիկ', 'Արփի', 'Բերկրուհի', 'Գոհար', 'Գոհարիկ', 'Դալար',
+                 'Դեղձանիկ', 'Դշխուհի', 'Դիցուհի', 'Եսթեր', 'Երանիկ', 'Երանուհի', 'Թագուհի', 'Ժպտանոյշ',
+                 'Իմաստուհի', 'Իշխանուհի', 'Իսկուհի', 'Լալա', 'Լիլիթ', 'Լուսաբեր', 'Լուսածին', 'Լուսարփի',
+                 'Լուսնթագ', 'Խոսրովանոյշ', 'Խոսրովիդուխտ', 'Ծովինար', 'Կատարինէ', 'Մարալ', 'Մարգարիտ']
+
+
+arm_boy_name = ['Աբգար', 'Աբէլ', 'Աբրահամ', 'Ադամ', 'Ազատ', 'Ալիշան', 'Ահարոն', 'Աղասի', 'Արծրուն', 'Արման',
+                'Արմենակ', 'Արմեն', 'Արշակ', 'Բագրատ', 'Բեգլար', 'Բենիամին', 'Գագիկ', 'Գալուստ', 'Գառնիկ',
+                'Գասպար', 'Գարեգին', 'Գեղամ', 'Գեղարդ', 'Խաժակ', 'Խաչատուր', 'Խաչիկ', 'Խոսրով', 'Կամսար',
+                'Կայծակ', 'Կարապետ', 'Կարեն', 'Կարպիս', 'Մհեր', 'Մուշեղ', 'Նազարեթ', 'Նժդեհ', 'Շիրազ', 'Շիրակ',
+                'Ոսկան', 'Ոսկեբերան', 'Ռազմիկ', 'Ռաֆֆի', 'Ռշտունի', 'Սերոբ', 'Սեւադա', 'Սեւակ', 'Սեւան', 'Սիմոն',
+                'Սիսակ', 'Սիրակ', 'Սիրական', 'Սիփան', 'Համազասպ', 'Համբարձում', 'Համո', 'Հայկ', 'Հայկազ',
+                'Հայկազուն', 'Ղազարոս', 'Ղուկաս', 'Մամիկոն', 'Մակար', 'Մարկոս', 'Մինաս', 'Մխիթար']
+
+
+def name_generator():
+    choice = random.choice((arm_girl_name, arm_boy_name))
+    if choice == arm_boy_name:
+        name_gender_ = 'Boy'
+    else:
+        name_gender_ = 'Girl'
+    # print(name_gender_)
+    name_gender = random.choice(choice)
+    return name_gender, name_gender_
+
+
 store_number = '1'
 LO_email = 'loii@sef.am'
 LO_password = 'Password2'
@@ -51,6 +82,22 @@ social_card_issue_date = '01-01-2010'
 passport_issue_date = '01-01-2022'
 passport_exp_date = '01-01-2032'
 PE_issue_day = '01-01-2000'
+comment = 'Lorem ipsum dolor sit amet, Римский император Константин I Великий, Լոռեմ իպսում դոլոր սիթ ամետ'
+name_generator_ = name_generator()
+firstName_gen = name_generator_[0]
+gender__ = name_generator_[1]
+
+patronymic_gen = random.choice(arm_boy_name)
+lastName_gen = random.choice(arm_surnames)
+arm_street = random.choice(arm_street_names)
+
+
+def a():
+    if gender__ == 'Boy':
+        g = 'MALE'
+    else:
+        g = 'FEMALE'
+    return g
 
 
 def add_potential_partner():
@@ -71,15 +118,15 @@ def add_potential_partner():
     addPotentialPartner_btn.click()
 
     legalName_fld = driver.find_element(By.CSS_SELECTOR, '[name="legalName"]')
-    legalName_fld.send_keys(generate_random_arm_word(make_arm_word, 5))
+    legalName_fld.send_keys(f"{firstName_gen} {lastName_gen} ԱՁ")
     taxCode_fld = driver.find_element(By.CSS_SELECTOR, '[name="taxCode"]')
     taxCode_fld.send_keys(generate_random_number(make_rand_number, 8))
     firstName_fld = driver.find_element(By.CSS_SELECTOR, '[name="firstName"]')
-    firstName_fld.send_keys(generate_random_arm_word(make_arm_word, 5))
+    firstName_fld.send_keys(firstName_gen)
     lastName_fld = driver.find_element(By.CSS_SELECTOR, '[name="lastName"]')
-    lastName_fld.send_keys(generate_random_arm_word(make_arm_word, 5))
+    lastName_fld.send_keys(lastName_gen)
     patronymic_fld = driver.find_element(By.CSS_SELECTOR, '[name="patronymic"]')
-    patronymic_fld.send_keys(generate_random_arm_word(make_arm_word, 5))
+    patronymic_fld.send_keys(patronymic_gen)
     BR_number = driver.find_element(By.XPATH, "(//input[@role='combobox'])[1]")
     BR_number.send_keys('e11', Keys.RETURN)
     region = driver.find_element(By.XPATH, "(//span[@class='ant-select-selection-item'])[2]")
@@ -91,11 +138,11 @@ def add_potential_partner():
     c_v_c_center = driver.find_element(By.XPATH, "(//div[contains(text(),'Կենտրոն')])[1]")
     c_v_c_center.click()
     address_str = driver.find_element(By.CSS_SELECTOR, '[name="addressDTOS.street"]')
-    address_str.send_keys(generate_random_arm_word(make_arm_word, 5))
+    address_str.send_keys(arm_street)
     address_house = driver.find_element(By.CSS_SELECTOR, '[name="addressDTOS.houseNumber"]')
     address_house.send_keys(generate_random_number(make_rand_number, 1))
     address_apartment = driver.find_element(By.CSS_SELECTOR, '[name="addressDTOS.apartmentNumber"]')
-    address_apartment.send_keys(generate_random_number(make_rand_number, 3))
+    address_apartment.send_keys(generate_random_number(make_rand_number, 2))
     save_btn = driver.find_element(By.CSS_SELECTOR, '[type="submit"]')
     save_btn.click()
 
@@ -119,8 +166,8 @@ def filtering_potential_partner():
 
     filterPartner = driver.find_element(By.CSS_SELECTOR, '[value="FILTER"]')
     filterPartner.click()
-    comment = driver.find_element(By.CSS_SELECTOR, '[name="decisionFeedback"]')
-    comment.send_keys(generate_random_arm_word(make_arm_word, 5))
+    comment_ = driver.find_element(By.CSS_SELECTOR, '[name="decisionFeedback"]')
+    comment_.send_keys(comment)
     uploadFile_1 = driver.find_element(By.XPATH, "(//input[contains(@type,'file')])[1]")
     uploadFile_1.send_keys(jpg)
     uploadFile_2 = driver.find_element(By.XPATH, "(//input[contains(@type,'file')])[2]")
@@ -165,9 +212,9 @@ def add_chain():
     pageLink = driver.find_element(By.CSS_SELECTOR, '[name="pageLink"]')
     pageLink.send_keys(url_of_store)
     brandName = driver.find_element(By.CSS_SELECTOR, '[name="brandName"]')
-    brandName.send_keys('sel.' + generate_random_eng_word(make_eng_word, 5))
+    brandName.send_keys('sel.' + names.get_last_name())
     nickname = driver.find_element(By.CSS_SELECTOR, '[name="nickname"]')
-    nickname.send_keys('sel.' + generate_random_eng_word(make_eng_word, 5))
+    nickname.send_keys('sel.' + names.get_last_name())
     emailAddr = driver.find_element(By.CSS_SELECTOR, '[name="email"]')
     emailAddr.send_keys(email_of_store)
     bankAccount = driver.find_element(By.CSS_SELECTOR, '[name="bankAccount"]')
@@ -182,8 +229,8 @@ def add_chain():
     residency.send_keys('Հայաստան', Keys.RETURN)
     nationality = driver.find_element(By.XPATH, "(//input[contains(@role,'combobox')])[3]")
     nationality.send_keys('ՀՀ', Keys.RETURN)
-    nationality = driver.find_element(By.CSS_SELECTOR, '[value="MALE"]')
-    nationality.click()
+    gender = driver.find_element(By.CSS_SELECTOR, f'[value="{a()}"]')
+    gender.click()
     childrenCount = driver.find_element(By.XPATH, "(//input[contains(@role,'combobox')])[4]")
     childrenCount.send_keys('1', Keys.RETURN)
     socialCard = driver.find_element(By.CSS_SELECTOR, '[name="socialCard"]')
--- misc.py
+++ misc.py
@@ -1,5 +1,21 @@
 import names
+import random
 
 
-for i in range(10):
-    print(names.get_full_name())
+# for i in range(1):
+#     print(names.get_full_name())
+# #
+# #
+# # convert string to brackets ''
+def convert(string):
+    li = list(string.split(" "))
+    return li
+
+str1 = "Ագապի Ազատուհի Այծեմնիկ Անի Անթառամ Աշխէն Ասթինէ Աստղիկ Արեգնազան Արշակուհի Արտեմիս Արփենիկ Արփի Բերկրուհի Գոհար Գոհարիկ Դալար Դեղձանիկ Դշխուհի Դիցուհի Եսթեր Երանիկ Երանուհի Թագուհի Ժպտանոյշ Իմաստուհի Իշխանուհի Իսկուհի Լալա Լիլիթ Լուսաբեր Լուսածին Լուսարփի Լուսնթագ Խոսրովանոյշ Խոսրովիդուխտ Ծովինար Կատարինէ Մարալ Մարգարիտ"
+
+print(convert(str1))
+
+
+
+
+
